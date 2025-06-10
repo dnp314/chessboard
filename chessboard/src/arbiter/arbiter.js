004e8bb795168f87e8ab24132d54f0e3adfc8913
Divyesh Parikh <dcparikh360@gmail.com>
2025-06-10 15:40:56 +0530

    Completed basic movements

--- chessboard/src/arbiter/arbiter.js
+++ chessboard/src/arbiter/arbiter.js
@@ -1,4 +1,4 @@
-import { getKnightMoves, getRookMoves } from "./getMoves"
+import { getBishopMoves, getKingMoves, getKnightMoves, getPawnCaptures, getPawnMoves, getQueenMoves, getRookMoves } from "./getMoves"
 
 const arbiter = {
   getRegularMoves: function({position,piece,rank,file}){
@@ -6,6 +6,17 @@ const arbiter = {
       return getRookMoves({position,piece,rank,file})
     if (piece.endsWith('N'))
       return getKnightMoves({position,rank,file})
+    if (piece.endsWith('B'))
+      return getBishopMoves({position,piece,rank,file})
+    if (piece.endsWith('Q'))
+      return getQueenMoves({position,piece,rank,file})
+    if (piece.endsWith('K'))
+      return getKingMoves({position,piece,rank,file})
+    if (piece.endsWith('P'))
+      return[ 
+        ...getPawnMoves({position,piece,rank,file}),
+        ...getPawnCaptures({position,piece,rank,file})
+      ]
   }
 }
 
